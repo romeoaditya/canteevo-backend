5ec58b8425265e7cb72921a01c6725ad4b49f8c9
romeoaditya <romeoaditya22@gmail.com>
2026-07-10 15:31:40 +0700

    fix config cloudinary

--- src/modules/cloudinary/cloudinary.service.ts
+++ src/modules/cloudinary/cloudinary.service.ts
@@ -4,6 +4,14 @@ import { Readable } from 'stream';
 
 @Injectable()
 export class CloudinaryService {
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
   async uploadImage(
     file: Express.Multer.File,
     folder: string = 'canteevo',
